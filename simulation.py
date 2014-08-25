5d3b6fb36b5c8c081a926e5048c670530e899772
Kevin Carrogan <kevin.carrogan@gmail.com>
2014-08-25 16:19:15 +0100

    Splitting out alien movement into a separate method.

--- simulation.py
+++ simulation.py
@@ -7,8 +7,11 @@ class Simulation(object):
 
     def run(self):
         while self.runs < 10000:
-            for alien in self.aliens:
-                alien.move()
+            self.move_aliens()
 
             self.runs += 1
         print 'Finished running'
+
+    def move_aliens(self):
+        for alien in self.aliens:
+            alien.move()
