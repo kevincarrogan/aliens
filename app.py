59c86582f7dff2e9b3a483365ce9975fb7c341bd
Kevin Carrogan <kevin.carrogan@gmail.com>
2018-06-21 01:52:48 +0100

    Randomly creating aliens.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/bin/python
+import random
 
 # A dictionary of cities.
 # The key being the name of the city and the corresponding dictionary value
@@ -24,11 +25,26 @@ cities = {
     },
 }
 
+
+def random_city(cities):
+    return random.choice(cities.keys())
+
+
+def generate_aliens(number_to_generate):
+    aliens = {}
+    for i in range(number_to_generate):
+        name = 'alien {}'.format(i)
+        city = random_city(cities)
+        aliens[name] = city
+    return aliens
+
+
 # A dictionary of aliens.
 # The key is the name of the alien and the value is the city the alien is currently
 # located in.
-aliens = {
-    'alien 1': 'Foo',
-    'alien 2': 'Bar',
-    'alien 3': 'Baz',
-}
+# aliens = {
+#    'alien 1': 'Foo',
+#    'alien 2': 'Bar',
+#    'alien 3': 'Baz',
+# }
+aliens = generate_aliens(10)
